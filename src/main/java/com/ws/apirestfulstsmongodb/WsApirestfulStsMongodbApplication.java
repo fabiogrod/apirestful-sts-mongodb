b15bfc3a6ce3d42076fda880dea44e0e2e7e949f
fabiogrod <fabiogrimello@gmail.com>
2020-11-30 12:21:51 -0300

    Aula 343 - Início do projeto

---
+++ src/main/java/com/ws/apirestfulstsmongodb/WsApirestfulStsMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.ws.apirestfulstsmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsApirestfulStsMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsApirestfulStsMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ws/apirestfulstsmongodb/WsApirestfulStsMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.apirestfulstsmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsApirestfulStsMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
