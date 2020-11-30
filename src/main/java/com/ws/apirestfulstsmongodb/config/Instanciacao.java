e5d3e9763cb8b8ec2c88b4fd61a854a113b0dcc9
fabiogrod <fabiogrimello@gmail.com>
2020-11-30 17:07:20 -0300

    Aula 346 - Operação de instanciação da base de dados

---
+++ src/main/java/com/ws/apirestfulstsmongodb/config/Instanciacao.java
@@ -0,0 +1,28 @@
+package com.ws.apirestfulstsmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.ws.apirestfulstsmongodb.dominios.Usuario;
+import com.ws.apirestfulstsmongodb.repositorios.RptrUsuario;
+
+@Configuration
+public class Instanciacao implements CommandLineRunner{
+
+	@Autowired private RptrUsuario rptrUsuario;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		rptrUsuario.deleteAll();
+		
+		Usuario paula = new Usuario( null, "Paula", "maria@gmail.com");
+		Usuario fernanda = new Usuario( null, "Fernanda", "fernanda@gmail.com");
+		Usuario andressa = new Usuario( null, "Andressa", "andressa@gmail.com");
+		
+		rptrUsuario.saveAll(Arrays.asList(paula, fernanda, andressa));
+	}
+}
--- src/main/java/com/ws/apirestfulstsmongodb/recursos/RcrsUsuario.java
+++ src/main/java/com/ws/apirestfulstsmongodb/recursos/RcrsUsuario.java
@@ -4,28 +4,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ws.apirestfulstsmongodb.dominios.Usuario;
-import com.ws.apirestfulstsmongodb.servicos.SrvcUsuario;
 
 @RestController @RequestMapping(value = "/usuarios")
 public class RcrsUsuario {
-	
-	@Autowired private SrvcUsuario srvcUsuario;
-	
+
 	// @GetMapping
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity < List<Usuario> > pesquisar() {
-//		Usuario paula = new Usuario("1", "Paula", "maria@gmail.com");
-//		Usuario fernanda = new Usuario("2", "Fernanda", "fernanda@gmail.com");
-				
-		List<Usuario> lista = srvcUsuario.pesquisar();
-//		lista.addAll(Arrays.asList(paula, fernanda));
+		Usuario paula = new Usuario("1", "Paula", "maria@gmail.com");
+		Usuario fernanda = new Usuario("2", "Fernanda", "fernanda@gmail.com");
+
+		List<Usuario> lista = new ArrayList<>();
+		lista.addAll(Arrays.asList(paula, fernanda));
 		return ResponseEntity.ok().body(lista);
 	}
 }
